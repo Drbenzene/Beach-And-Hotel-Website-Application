bd4c28edc08b5a2cf597aab77bae996571dc0170
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2022-07-26 05:04:21 -0500

    Fixing Bugs

---
+++ src/Context.jsx
@@ -0,0 +1,23 @@
+import  React from "react";
+
+
+const GlobalContext = React.createContext();
+
+const Provider = (props) => {
+
+
+
+
+    const states = {
+        
+    }
+    return (
+        <GlobalContext.Provider value={states} >
+            {props.children}
+        </GlobalContext.Provider>
+    )
+}
+
+const Consumer = GlobalContext.Consumer
+
+export { GlobalContext, Provider, Consumer };
